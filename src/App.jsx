367c95b015e2133af986ca614205de65b0aa1e4c
Syddev <sydneysantos176@gmail.com>
2025-05-24 19:56:59 +0800

    edit again the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ExpenseTracker from "./ExpenseTracker";
 import Home from "./Home";
 import History from "./History";
@@ -6,13 +6,11 @@ import History from "./History";
 function App() {
   return (
     <BrowserRouter basename="/Expense-Tracker">
-      <Router>
-        <Routes>
-          <Route path="/" element={<ExpenseTracker />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<ExpenseTracker />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </BrowserRouter>
   );
 }
